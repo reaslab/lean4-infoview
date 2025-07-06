cd6526138bc337e99f60c978a31c348beb87bfb9
Alissa Tung <alissa-tung@outlook.com>
2025-07-06 14:27:12 +0800

    hack: local tachyons.css

--- lean4-infoview/src/infoview/main.tsx
+++ lean4-infoview/src/infoview/main.tsx
@@ -3,9 +3,10 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import type { DidCloseTextDocumentParams, DocumentUri, Location } from 'vscode-languageserver-protocol'
 
-import '@vscode/codicons/dist/codicon.css'
-import '@vscode/codicons/dist/codicon.ttf'
-import 'tachyons/css/tachyons.css'
+// FIXME
+// import '@vscode/codicons/dist/codicon.css'
+// import '@vscode/codicons/dist/codicon.ttf'
+import './tachyons.css'
 import './highlightjs.css'
 import './index.css'
 
